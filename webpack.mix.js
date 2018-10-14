e9094ac7f8d44f79fc272fe5e048ca87f647e346
Ali PF Njie <ali.njie@qcell.gm>
2018-10-14 15:16:12 +0000

    starter

---
+++ webpack.mix.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const mix = require("laravel-mix");
+const tailwindcss = require("tailwindcss");
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
+mix.config.vue.esModule = true;
+
+mix
+  .js("resources/js/app.js", "public/js")
+  .sass("resources/sass/app.scss", "public/css")
+  .options({
+    processCssUrls: false,
+    postCss: [tailwindcss("./tailwind.js")]
+  })
+
+  .sourceMaps()
+  .disableNotifications();
+
+if (mix.inProduction()) {
+  mix.version();
+
+  mix.extract([
+    "vue",
+    "vform",
+    "axios",
+    "vuex",
+    "jquery",
+    "popper.js",
+    "vue-i18n",
+    "vue-meta",
+    "js-cookie",
+    "bootstrap",
+    "vue-router",
+    "sweetalert2",
+    "vuex-router-sync",
+    "@fortawesome/fontawesome",
+    "@fortawesome/vue-fontawesome",
+    "tailwindcss"
+  ]);
+}
+
+mix.webpackConfig({
+  plugins: [
+    // new BundleAnalyzerPlugin()
+  ],
+  resolve: {
+    extensions: [".js", ".json", ".vue"],
+    alias: {
+      "~": path.join(__dirname, "./resources/js")
+    }
+  },
+  output: {
+    chunkFilename: "js/[name].[chunkhash].js",
+    publicPath: mix.config.hmr ? "//localhost:8080" : "/"
+  }
+});
